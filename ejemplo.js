3e066bd7fa6ee64f9235e59ea1e0836965fe1fb2
Chris <chrischillogallid64@outlook.com>
2020-10-06 22:25:29 -0500

    chore: api to speak

--- ejemplo.js
+++ ejemplo.js
@@ -1,1 +1,6 @@
-console.log('dsd');
+console.log(window);
+
+let texto = 'Hola Maggot, eres chido xd xd'
+const hablar = (hablar) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+
+hablar(texto);
