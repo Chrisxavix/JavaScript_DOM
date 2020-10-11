a48387d457491a158d733c58250925a8796878d3
Chris <chrischillogallid64@outlook.com>
2020-10-10 23:28:20 -0500

    chore: html

--- dom.js
+++ dom.js
@@ -1,9 +1,14 @@
 /* ------------------ JAVASCRIPT - DOM ----------------- */
 /* 
 1) Introducción, line 
-1) Nodos, Elementos y Selectores, line 
-2) Atributos y Data-Attributes, line 
- */
+2) Nodos, Elementos y Selectores, line 
+3) Atributos y Data-Attributes, line
+4) Estilos y Variables CSS 
+5) Clases CSS 
+6) Texto y HTML
+7) Recorriendo el DOM
+*/
+
 /* 1) --------------- Introducción ------------------- */
 /* Hacer que la PC reconozca la cadena y emita audio */
 /* let texto = 'Hola Maggot, eres chido xd xd'
@@ -76,3 +81,60 @@ console.log($linkDom.dataset.id);
 $linkDom.dataset.id = 7;
 console.log($linkDom.dataset.id); */
 /* 3) ------- Atributos y Data-Attributes  ----------- */
+
+/* 4) --------- Estilos y Variables CSS   ------------ */
+/* const $html = document.documentElement;
+const $body = document.body;
+//Acceder a las variables
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
+let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color")
+console.log($html);
+console.log($body);
+console.log(varDarkColor);
+console.log(varYellowColor);
+//Cambiar fondo a la página desde las variable en HTML
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor; */
+/* 4) --------- Estilos y Variables CSS   ------------ */
+
+/* 5) ---------------- Clases CSS -------------------- */
+/* const $card = document.querySelector(".card");
+$card.classList.add("rotate-45");
+$card.classList.add("opacity-80", "sepia");
+$card.classList.replace("rotate-45", "rotate-135");
+$card.classList.remove("opacity-80", "sepia");
+$card.classList.toggle("opacity-80", "sepia"); */
+/* 5) ---------------- Clases CSS -------------------- */
+
+/* 6) --------------- Texto y HTML -------------------- */
+/* Reemplazar el texto que tiene el html con (innerHTML) */
+/* const $whatisDOM = document.getElementById("que-es");
+let text = "<p> EL Dom es <br> parte de la Interfaz.</p>" +
+"<p>El diseño va en css.</p>";
+// $whatisDOM.innerHTML = text;
+// Elimina el menú de p, y cada parrafo toma posición
+$whatisDOM.outerHTML = text; */
+/* 6) --------------- Texto y HTML -------------------- */
+
+/* 7) ------------ Recorriendo el DOM ----------------- */
+/* Enfocado a las etiquetas de html */
+/* const $cards = document.querySelector(".cards");
+console.log($cards);
+// Accede a los hijos
+console.log($cards.children);
+// Accede a un hijo
+console.log($cards.children[2]);
+// Acceder al primero o último elemento
+// Con esos dos, toma el espacio como text
+console.log($cards.firstChild);
+console.log($cards.lastChild);
+// Sin el espacio que existe 
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+// Ya no dentro de los elemntos de .card, sino las
+// etiquetas que anteceden o van después 
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+// Métodos cercanos
+console.log($cards.children[3].closest("section")); */
+/* 7) ------------ Recorriendo el DOM ----------------- */
