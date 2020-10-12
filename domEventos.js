b5ac5b69a4f5c7fa2b125a43ab74e3f1645f0ec8
Chris <chrischillogallid64@outlook.com>
2020-10-11 23:08:35 -0500

    chore: remove events

--- domEventos.js
+++ domEventos.js
@@ -1,14 +1,14 @@
 /* ------------------ JAVASCRIPT - DOM ----------------- */
 /* 
 1) Eventos, line 
-2) Nodos, Elementos y Selectores, line 
-3) Atributos y Data-Attributes, line
-4) Estilos y Variables CSS 
-5) Clases CSS 
-6) Texto y HTML
-7) Recorriendo el DOM
-8) Creando Elementos y Fragmentos
-9) Templates HTML
+2) Eventos con Parámetros y Remover Eventos, line 
+3) 
+4)  
+5) 
+6) 
+7) 
+8) 
+9) 
 */
 
 /* 1) --------------------- Eventos -------------------- */
@@ -50,3 +50,38 @@ $eventomultiple.addEventListener("click", (e) => {
     console.log(e.target);
 }) */
 /* 1) --------------------- Eventos -------------------- */
+
+/* 2) ---- Eventos con Parámetros y Remover Eventos ---- */
+/* const $eventomultiple = document.getElementById("evento-multiple");
+function saludar(name="desconocido") {
+    alert(`Hola ${name}`);
+}
+function holaMundo() {
+    alert('Hola mundo');
+}
+$eventomultiple.addEventListener("click", holaMundo)
+$eventomultiple.addEventListener("click", (e) => {
+    alert('Hola mundo múltiple.');
+})
+$eventomultiple.addEventListener("click", () => saludar('Chris'));
+ */
+
+/* Deshabilitar botones
+const $eventoremover = document.getElementById("evento-remover");
+// Función declarada 
+function remover2(e) {
+    alert(`Removiendo el evento de tipo ${e.type}`);
+    $eventoremover.removeEventListener("dblclick", remover2);
+}
+// Función expresada 
+const remover = (e) => {
+    alert(`Removiendo el evento de tipo ${e.type}`);
+    $eventoremover.removeEventListener("dblclick", remover);
+    $eventoremover.disabled = true;
+}
+// Con función expresada
+$eventoremover.addEventListener("dblclick", remover);
+// Con función declarada
+// $eventoremover.addEventListener("dblclick", remover2); */
+/* 2) ---- Eventos con Parámetros y Remover Eventos ---- */
+
